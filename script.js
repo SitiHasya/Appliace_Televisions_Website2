120bb0aef9929fe04739d7621e5289ec7f8c1ea9
Hasya <104381534@students.swinburne.edu.my>
2025-09-17 11:32:34 +0800

    comm

---
+++ script.js
@@ -0,0 +1,11 @@
+function goToHome() {
+    window.location.href = "page1Home.html";
+}
+
+function goToTelevisions() {
+    window.location.href = "page2Tv.html";
+}
+
+function goToAboutUs() {
+    window.location.href = "page3AboutUs.html";
+}
